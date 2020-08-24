b314fa8ae4fb52c1b56e5237acb4b3c9dd6a0b40
mkopsho <michael.kopsho@gmail.com>
2020-08-24 13:36:48 -0400

    Add map listener and createAndPlaceMarker() function

--- trout-slayer-frontend/src/index.js
+++ trout-slayer-frontend/src/index.js
@@ -6,12 +6,28 @@ fetch(`${BACKEND_URL}/test`)
   .then((parsedResponse) => console.log(parsedResponse))
   .catch((error) => console.log(error))
 
-// Testing maps
-let map
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+function createMap() {
+  let map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 39.809, lng: -98.555 },
+    zoom: 5,
+  })
+  map.addListener('click', function (e) {
+    createAndPlaceMarker(e.latLng, map)
+    console.log(e)
   })
 }
+
+function createAndPlaceMarker(latLng, map) {
+  console.log('placing marker')
+  let marker = new google.maps.Marker({
+    position: latLng,
+    map: map,
+  })
+  map.panTo(latLng)
+}
+
+// To do: try geolocation
+
+document.addEventListener('DOMContentLoaded', function () {
+  createMap()
+})
