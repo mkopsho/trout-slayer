e2c8a08b2996a80025c859bdb66ddda1f0d954c1
mkopsho <michael.kopsho@gmail.com>
2020-08-24 12:41:20 -0400

    Setup Google Maps Javascript API and tested with sample code/styling

--- trout-slayer-frontend/src/index.js
+++ trout-slayer-frontend/src/index.js
@@ -5,3 +5,13 @@ fetch(`${BACKEND_URL}/test`)
   .then((response) => response.json())
   .then((parsedResponse) => console.log(parsedResponse))
   .catch((error) => console.log(error))
+
+// Testing maps
+let map
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  })
+}
