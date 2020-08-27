97f50e4b1db14b2069463467bc8bb81efcc395d0
mkopsho <michael.kopsho@gmail.com>
2020-08-27 11:59:29 -0400

    Scrapping edit/delete in the interest of time; keeping this as a log of the changes that need to be made

--- trout-slayer-frontend/index.js
+++ trout-slayer-frontend/index.js
@@ -32,6 +32,7 @@ function renderSavedMarkers(markers, map) {
     const newMarker = new google.maps.Marker({
       position: latLng,
       label: marker.user_id.toString(),
+      id: marker.id,
       icon: MAP_ICONS + 'fishing.png',
     })
     const markerContent = `
@@ -40,8 +41,6 @@ function renderSavedMarkers(markers, map) {
       <strong>Fish:</strong> <p>${marker.fish_type}</p>
       <strong>Lure:</strong> <p>${marker.lure_and_bait}</p>
       <strong>Weather:</strong> <p>${marker.weather_conditions}</p>
-      <button id="edit-button" type="button">Edit</button>
-      <button id="delete-button" type="button">Delete</button>
       `
     const infoWindow = new google.maps.InfoWindow({
       content: markerContent,
@@ -109,8 +108,6 @@ function formListenerAndValueGatherer(latLng, infoWindow) {
         <strong>Fish:</strong> <p>${newMarkerFish}</p>
         <strong>Lure:</strong> <p>${newMarkerLure}</p>
         <strong>Weather:</strong> <p>${newMarkerWeather}</p>
-        <button id="edit-button" type="button">Edit</button>
-        <button id="delete-button" type="button">Delete</button>
       `
       infoWindow.setContent(newMarkerContent)
       let saveArgs = {
@@ -125,38 +122,6 @@ function formListenerAndValueGatherer(latLng, infoWindow) {
       e.preventDefault()
     })
   })
-}
-
-// More marker stuff -- move to markersAdapter.js
-function editAndDeleteListeners(marker, infoWindow) {
-  infoWindow.addListener('domready', function () {
-    const editButton = document.querySelector('#edit-button')
-    const deleteButton = document.querySelector('#delete-button')
-    editButton.addEventListener('click', function (e) {
-      editMarker(marker)
-      e.preventDefault()
-    })
-    deleteButton.addEventListener('click', function (e) {
-      deleteMarker(marker)
-      e.preventDefault()
-    })
-  })
-}
-
-function editMarker(marker) {
-  if (session.id == marker.label) {
-    console.log('can be edited by user')
-  } else {
-    errorHandler('You do not have the correct permissions to do that.')
-  }
-}
-
-function deleteMarker(marker) {
-  if (session.id == marker.label) {
-    console.log('can be deleted by user')
-  } else {
-    errorHandler('You do not have the correct permissions to do that.')
-  }
 }
 
 // Signup and login
